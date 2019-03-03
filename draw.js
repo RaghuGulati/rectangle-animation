867b32e2865d8dc5dfc35d3583e4a0e3bf22bebc
RaghuGulati <45628486+RaghuGulati@users.noreply.github.com>
2019-03-03 16:42:53 +0530

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,5 +1,5 @@
 function draw() {
-<canvas id="canvas" width="150" height="150">
+
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
     var context = canvas.getContext('2d');
@@ -8,6 +8,5 @@ function draw() {
     context.clearRect(40,40,60,60);
     context.strokeRect(45,45,50,50);
   }
-</canvas>
 
 }
